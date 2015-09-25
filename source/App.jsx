f64911b848c012cc1298b29a3fde53e928fc2d75
sldhana <sldhana@gmail.com>
2015-09-24 23:59:06 -0700

    Update App.jsx

--- source/App.jsx
+++ source/App.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
 var WeatherApp = require('./components/WeatherApp.jsx');
 
-
+//Render to the DOM
 React.render(<WeatherApp />, document.getElementById('weather-application'));
